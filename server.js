a7b1188e010413e84b7f1bd5606139f2da52576c
Kasean Cobbs-Walton <smartmoneysensei@gmail.com>
2023-02-09 20:56:30 -0800

    completed Contact Us page and successful form using formsubmit.com & added learn pages for future coding

--- server.js
+++ server.js
@@ -38,6 +38,26 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(intialPath, "register.html"));
 })
 
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(intialPath, "contact.html"));
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(intialPath, "about.html"));
+})
+
+app.get('/mind', (req, res) => {
+    res.sendFile(path.join(intialPath, "sentimental.html"));
+})
+
+app.get('/skill', (req, res) => {
+    res.sendFile(path.join(intialPath, "technical.html"));
+})
+
+app.get('/research', (req, res) => {
+    res.sendFile(path.join(intialPath, "fundamental.html"));
+})
+
 
 app.listen(3000, (req, res) => {
     console.log('listening on port 3000......')
