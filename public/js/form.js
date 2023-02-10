369905fd0384d85bff14ba953380320e69644d25
Kasean Cobbs-Walton <smartmoneysensei@gmail.com>
2023-02-10 01:33:49 -0800

    Created functions name, email, password, & submit button for coding funtionality

--- public/js/form.js
+++ public/js/form.js
@@ -14,3 +14,10 @@ window.onload = () => {
     }
 }
 
+// form validation
+
+const name = document.querySelector('.name') || null;
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const submitBtn = document.querySelector('.submit-btn');
+
