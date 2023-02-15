869949a1bfd70bccf5956350a88228a059f2e177
Kasean Cobbs-Walton <smartmoneysensei@gmail.com>
2023-02-14 19:58:23 -0800

    changed host back to og in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1/5432',
+        host: '127.0.0.1',
         user: 'postgres',
         password: 'Zarian',
         database: 'dojo'
