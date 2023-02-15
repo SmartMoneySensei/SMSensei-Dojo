2219c38d5ba5f9f99362715567c409deecc691e4
Kasean Cobbs-Walton <smartmoneysensei@gmail.com>
2023-02-14 20:46:23 -0800

    fixed error recieved from running app by requiring cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const knex = require('knex');
+const cors = require('cors')
 const { response } = require('express');
 require('dotenv').config()
 
